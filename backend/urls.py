aa164b5b809919319b5d8879d24ecdd8833e6017
rahul ghetia <rahulghetia67@gmail.com>
2020-09-03 14:46:06 +0530

    Added Remaining Files

---
+++ backend/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.welcome, name="welcome"),
+    path('signup', views.signup, name="signup"),
+    path('login', views.login, name="login"),
+]
